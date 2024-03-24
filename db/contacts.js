3f0b1b59728fbe6af9373c7925a2fcb7447934ee
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-24 23:02:58 +0100

    comment sremoved

--- db/contacts.js
+++ db/contacts.js
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Obliczanie ścieżki __dirname w kontekście ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
