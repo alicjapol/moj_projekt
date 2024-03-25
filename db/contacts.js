1ad404a6904a94acbd45b3aae9c74718c2e40d5e
alicjapol <alicjaxpoltorak@gmail.com>
2024-03-25 20:55:28 +0100

    trycatch added where missing

--- db/contacts.js
+++ db/contacts.js
@@ -1,6 +1,7 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,44 +20,73 @@ async function readContactsFile() {
 
 async function writeContactsToFile(contacts) {
   try {
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+      "utf-8"
+    );
   } catch (err) {
     console.error("Błąd podczas zapisu pliku z kontaktami:", err);
   }
 }
 
 async function listContacts() {
-  const contacts = await readContactsFile();
-  console.table(contacts);
+  try {
+    const contacts = await readContactsFile();
+    console.table(contacts);
+  } catch (err) {
+    console.error("Błąd przy pobieraniu listy", err);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await readContactsFile();
-  const contact = contacts.find(contact => contact.id === contactId.toString());
-  if (!contact) {
-    console.log(`Nie znaleziono kontaktu o ID: ${contactId}`);
-    return;
+  try {
+    const contacts = await readContactsFile();
+    const contact = contacts.find(
+      (contact) => contact.id === contactId.toString()
+    );
+    if (!contact) {
+      console.log(`Nie znaleziono kontaktu o ID: ${contactId}`);
+      return;
+    }
+    console.log(contact);
+  } catch (err) {
+    console.error("Błąd przy pobieraniu danych kontaktu po numerze ID", err);
   }
-  console.log(contact);
 }
 
 async function removeContact(contactId) {
-  const contacts = await readContactsFile();
-  const filteredContacts = contacts.filter(contact => contact.id !== contactId.toString());
-  if (contacts.length === filteredContacts.length) {
-    console.log(`Nie znaleziono kontaktu o ID: ${contactId} do usunięcia.`);
-    return;
+  try {
+    const contacts = await readContactsFile();
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId.toString()
+    );
+    if (contacts.length === filteredContacts.length) {
+      console.log(`Nie znaleziono kontaktu o ID: ${contactId} do usunięcia.`);
+      return;
+    }
+    await writeContactsToFile(filteredContacts);
+    console.log(`Usunięto kontakt o ID: ${contactId}`);
+  } catch (err) {
+    console.error("Błąd przy usuwaniu kontaktu!", err);
   }
-  await writeContactsToFile(filteredContacts);
-  console.log(`Usunięto kontakt o ID: ${contactId}`);
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await readContactsFile();
-  const newContact = { id: Date.now().toString(), name, email, phone };
-  contacts.push(newContact);
-  await writeContactsToFile(contacts);
-  console.log(`Dodano kontakt: ${name}`);
+  try {
+    const contacts = await readContactsFile();
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await writeContactsToFile(contacts);
+    console.log(`Dodano kontakt: ${name} z ID: ${newContact.id}`);
+  } catch (err) {
+    console.error("Błąd przy dodawaniu nowego kontaktu.", err);
+  }
 }
 
 export { listContacts, getContactById, removeContact, addContact };
